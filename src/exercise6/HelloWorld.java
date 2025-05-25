3ee0a9e4cbda552ffd093904b45d5ed721787c8e
NishaVijayamma <vsn4891@gmail.com>
2025-05-25 22:24:16 +0200

    Update HelloWorld.java
    Cats and Dogs

--- src/exercise6/HelloWorld.java
+++ src/exercise6/HelloWorld.java
@@ -2,7 +2,7 @@ package exercise6;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Cat!");
+        System.out.println("Hello Cats!");
         System.out.println("Hello Dog!");
 
         System.out.println("Hello World!");
